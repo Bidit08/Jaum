b528f76b241e6296642eead95625590f2d22ef7e
Bidit08 <np03cs4a230435@heraldcollege.edu.np>
2026-05-30 13:21:59 +0545

    Update backend URL for socket connection

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://jaum-t3no.onrender.com/api";
 
 // Create a single global socket instance but don't connect automatically
 export const socket = io(BACKEND_URL, {
